afd03ac15cee561ef5250e71422dcea7b0952e1a
Martinhunt100 <martin@digital-gravy.com>
2025-11-05 15:35:47 +0000

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+return (
+<div className="text-center space-y-4">
+<h1 className="font-serif text-4xl">Page not found</h1>
+<p>Let’s get you back to Christmas magic.</p>
+<a className="btn" href="/">Go Home</a>
+</div>
+);
+}
