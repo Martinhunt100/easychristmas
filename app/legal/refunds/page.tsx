af5751f3aa9c8f331912b68efe6f4c3cc245128f
Martinhunt100 <martin@digital-gravy.com>
2025-11-05 15:42:30 +0000

    Create page.tsx

---
+++ app/legal/refunds/page.tsx
@@ -0,0 +1,8 @@
+export default function Refunds() {
+return (
+<div className="prose max-w-3xl">
+<h1>Refunds</h1>
+<p>Digital items are generally non-refundable once downloaded. If you have an issue with a file, contact us within 7 days and we’ll repair or replace it. If the file has not been downloaded, a refund may be issued at our discretion.</p>
+</div>
+);
+}
