95884be28db61158fa557fb42e7ab2c638c7c06d
Martinhunt100 <martin@digital-gravy.com>
2025-11-05 15:47:21 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: { bodySizeLimit: '2mb' } }
+experimental: { serverActions: { bodySizeLimit: '2mb' } },
+async headers() {
+return [
+{
+source: '/(.*)',
+headers: [
+{ key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+{ key: 'X-Content-Type-Options', value: 'nosniff' },
+{ key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' }
+]
+}
+];
+}
 };
 export default nextConfig;
