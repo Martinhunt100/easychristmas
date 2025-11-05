94169247cb229ad7402f8bcf11cc41f36bc4a70c
Martinhunt100 <martin@digital-gravy.com>
2025-11-05 15:44:53 +0000

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+return (
+<div className="prose max-w-3xl">
+<h1>About Easy Christmas</h1>
+<p>We design beautiful, time-saving Christmas printables and templates so you can create magic fast. All products are crafted in the USA with teachers, families, and makers in mind.</p>
+</div>
+);
+}
